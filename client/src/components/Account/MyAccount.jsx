f55d53e82fc3507074eec6608b46cbfbcf8794c1
My Grandin <my.grandin@gmail.com>
2022-11-14 09:46:03 +0100

    Css-form

--- client/src/components/Account/MyAccount.jsx
+++ client/src/components/Account/MyAccount.jsx
@@ -18,35 +18,30 @@ const MyAccount = () => {
   }, []);
   return (
     <>
-        <div className="myaccountheader">
+      <form className="myaccountform">
+        <div className="myaccountinput">
           <div></div>
           <h2>Mitt konto</h2>
         </div>
-        <form className="myaccountform">
-          <div className="myaccountinput">
-            <label htmlFor="firstname">Förnamn:</label>
-            <input
-              type="text"
-              name="firstname"
-              disabled
-              value={user.firstname}
-            />
-          </div>
-          <div className="myaccountinput">
-            <label htmlFor="lastname">Efternamn:</label>
-            <input type="text" name="lastname" disabled value={user.lastname} />
-          </div>
-          <div className="myaccountinput">
-            <label htmlFor="email">E-postadress:</label>
-            <input type="text" name="email" disabled value={user.email} />
-          </div>
-          <div className="myaccountinput">
-            <div></div>
-            <button className="myaccountbutton">
-              <Link to="/admin">Ändra lösenord</Link>
-            </button>
-          </div>
-        </form>
+        <div className="myaccountinput">
+          <label htmlFor="firstname">Förnamn:</label>
+          <input type="text" name="firstname" disabled value={user.firstname} />
+        </div>
+        <div className="myaccountinput">
+          <label htmlFor="lastname">Efternamn:</label>
+          <input type="text" name="lastname" disabled value={user.lastname} />
+        </div>
+        <div className="myaccountinput">
+          <label htmlFor="email">E-postadress:</label>
+          <input type="text" name="email" disabled value={user.email} />
+        </div>
+        <div className="myaccountinput">
+          <div></div>
+          <button className="myaccountbutton">
+            <Link to="/admin">Ändra lösenord</Link>
+          </button>
+        </div>
+      </form>
     </>
   );
 };
