5d79639b07994495a10bbfeadc56470f3a748886
My Grandin <my.grandin@gmail.com>
2022-11-16 21:11:58 +0100

    Test updated validation

--- client/src/components/test/Test.jsx
+++ client/src/components/test/Test.jsx
@@ -1,48 +1,63 @@
 import React, { useState } from "react";
 import Papa from "papaparse";
-import "./test.css"
+import "./test.css";
 const Test = () => {
-  const [file, setFile] = useState();
+  const [file, setFile] = useState({});
   const [status, setStatus] = useState("Redo");
+  const [error, setError] = useState("");
+  const [name, setName] = useState("");
+  const [showMessage, setShowMessage] = useState("");
 
   const fileReader = new FileReader();
 
   const handleOnChange = (event) => {
+    console.log(event);
     // Passing file data (event.target.files[0]) to parse using Papa.parse
     Papa.parse(event.target.files[0], {
       header: true,
       skipEmptyLines: true,
       complete: function (results) {
-        console.log(results.data);
+        setFile(results.data);
       },
     });
   };
   const handleOnSubmit = (e) => {
     e.preventDefault();
+    if (!new RegExp(/^[A-Öa-ö\d]+$/).test(name) || name.length > 100) {
+      setError(
+        "Namn får endast innehålla bokstäver och siffror och vara max 100 tecken långt"
+      );
+      setShowMessage(true);
+    } else {
+      console.log("skicka objekt");
+      if (file) {
+        fileReader.onload = function (event) {
+          const csvOutput = event.target.result;
+        };
 
-    if (file) {
-      fileReader.onload = function (event) {
-        const csvOutput = event.target.result;
-      };
-
-      fileReader.readAsText(file);
+        fileReader.readAsText(file);
+      }
     }
   };
   return (
     <>
       <form className="testform">
-      <div className="myaccountinput">
+        <div className="myaccountinput">
           <div></div>
           <h2>Ladda upp test</h2>
         </div>
         <div className="testinput">
-          <label htmlFor="lastname">Namn på test:</label>
-          <input type="text" name="lastname" />
+          <label htmlFor="name">Namn på test:</label>
+          <input
+            type="text"
+            name="name"
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
         <div className="testinput">
-          <p >Ladda fil:</p>
+          <p>Ladda fil:</p>
           <input
-          className="test-csv-input"
+            className="test-csv-input"
             type={"file"}
             id={"csvFileInput"}
             accept={".csv"}
@@ -54,7 +69,8 @@ const Test = () => {
         <div className="test-status-row">
           <p>Status:</p>
           <p className="test-status">{status}</p>
-          <button className="testbutton"
+          <button
+            className="testbutton"
             onClick={(e) => {
               handleOnSubmit(e);
             }}
@@ -63,6 +79,10 @@ const Test = () => {
           </button>
         </div>
       </form>
+      <div>
+        {" "}
+        {showMessage ? <p className="signinerrormessage">{error}</p> : ""}
+      </div>
     </>
   );
 };
