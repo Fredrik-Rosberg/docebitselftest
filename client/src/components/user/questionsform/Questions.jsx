b7201b8d2f2b52ed2baf3dfa24f9d1bfff05488a
My Grandin <my.grandin@gmail.com>
2022-11-28 13:18:42 +0100

    Update Questions.jsx

--- client/src/components/user/questionsform/Questions.jsx
+++ client/src/components/user/questionsform/Questions.jsx
@@ -2,7 +2,11 @@ import React, { useState, useEffect } from "react";
 import "./questions.css";
 import { useTimer } from "react-timer-hook";
 import UseTimer from "./Timer";
+import { BsStar } from "react-icons/bs";
+import { BsStarFill } from "react-icons/bs";
 
+// AiOutlineStar
+// AiFillStar
 const Questions = () => {
   const time = new Date();
   time.setSeconds(time.getSeconds() + 12 * 60);
@@ -10,16 +14,16 @@ const Questions = () => {
   const alpha = Array.from(Array(11)).map((e, i) => i + 65);
   const alphabet = alpha.map((x) => String.fromCharCode(x).toLowerCase());
   const [checked, SetChecked] = useState([]);
-  const [counter, SetCounter]=useState(0)
+  const [counter, SetCounter] = useState(0);
+  const [star, setStar] = useState(false);
 
   useEffect(() => {
-   let count=0;
+    let count = 0;
     alphabet.map((item) =>
-    question["frågealternativ" + item] != "" ? (count=count+1)  : ("")
-      
+      question["frågealternativ" + item] != "" ? (count = count + 1) : ""
     );
-    SetCounter(count)
-  },[question]);
+    SetCounter(count);
+  }, [question]);
 
   function handleNext() {
     SetQuestion(JSON.parse(localStorage.getItem(question.fråganr + 1)));
@@ -28,16 +32,18 @@ const Questions = () => {
 
   function handlePrevious() {
     SetQuestion(JSON.parse(localStorage.getItem(question.fråganr - 1)));
-    let arrayFalse=Array(counter).fill(false)
-    const getFromSession=sessionStorage.getItem(question.fråganr -1 )
-    let test=getFromSession.split(",")
-    test.map((item)=>item=="true"? arrayFalse[item.index]=true:arrayFalse[item.index]=false)
+    let arrayFalse = Array(counter).fill(false);
+    const getFromSession = sessionStorage.getItem(question.fråganr - 1);
+    let test = getFromSession.split(",");
+    test.map((item) =>
+      item == "true"
+        ? (arrayFalse[item.index] = true)
+        : (arrayFalse[item.index] = false)
+    );
 
-
-
-    console.log(test)
-    console.log(arrayFalse) 
-    console.log(getFromSession)
+    console.log(test);
+    console.log(arrayFalse);
+    console.log(getFromSession);
   }
 
   function handleAbort() {}
@@ -45,13 +51,17 @@ const Questions = () => {
 
   function handleChecked(index, item) {
     let array = [...checked];
-    array[index] = item
+    array[index] = item;
     SetChecked(array);
   }
 
   useEffect(() => {
     console.log(checked);
   });
+
+  const handleStar = () => {
+    setStar((star) => !star);
+  };
 
   return (
     <>
@@ -63,7 +73,11 @@ const Questions = () => {
         </div>
         <div className="questionscontainer">
           <div className="questionsblock">
-            <div>star</div>
+            {star ? (
+              <BsStarFill onClick={handleStar} />
+            ) : (
+              <BsStar onClick={handleStar} />
+            )}
             <div className="questionsblockinner">
               <h3 className="questionnumber">Fråga {question.fråganr}</h3>
               <div className="question setfontsize">{question.fråga}</div>
@@ -75,7 +89,7 @@ const Questions = () => {
                   question["frågealternativ" + item] != "" ? (
                     <div key={index} className="questiongrid">
                       <input
-                      value={item}
+                        value={item}
                         onChange={() => handleChecked(index, item)}
                         type="checkbox"
                       ></input>
