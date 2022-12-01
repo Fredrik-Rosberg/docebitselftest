7049953bf59b5ffdc00e11939b8f42eeee1283a5
frippeosd <fredrik_rosberg@hotmail.com>
2022-12-01 11:57:09 +0100

    set letters to answerarray

--- client/src/components/user/questionsform/Questions.jsx
+++ client/src/components/user/questionsform/Questions.jsx
@@ -2,11 +2,8 @@ import React, { useState, useEffect } from "react";
 import "./questions.css";
 import { useTimer } from "react-timer-hook";
 import UseTimer from "./Timer";
-import { BsStar } from "react-icons/bs";
-import { BsStarFill } from "react-icons/bs";
+import { BsStar, BsStarFill } from "react-icons/bs";
 
-// AiOutlineStar
-// AiFillStar
 const Questions = () => {
   const time = new Date();
   time.setSeconds(time.getSeconds() + 12 * 60);
@@ -14,21 +11,22 @@ const Questions = () => {
   const alpha = Array.from(Array(11)).map((e, i) => i + 65);
   const alphabet = alpha.map((x) => String.fromCharCode(x).toLowerCase());
   const [checked, SetChecked] = useState(Array(11).fill(false));
-  const [counter, SetCounter] = useState(0);
-  const [star, setStar] = useState(false);
-  const localStorageCount = Array.from({ length: localStorage.length }, (v, i) => i);
-  const [starInDropDown, setStarInDropDown] = useState(Array(localStorage.length).fill(false));
+  const localStorageCount = Array.from(
+    { length: localStorage.length },
+    (v, i) => i
+  );
+  const [starInDropDown, setStarInDropDown] = useState(
+    Array(localStorage.length).fill(false)
+  );
 
   useEffect(() => {
     getFromSession(-question.fråganr + 1);
-    console.log(localStorageCount);
-    console.log(starInDropDown);
   }, [starInDropDown]);
 
   function handleNext() {
     let array = Array(11).fill(false);
     SetChecked(array);
-    getFromSession(question.fråganr +1);
+    getFromSession(question.fråganr + 1);
     SetQuestion(JSON.parse(localStorage.getItem(question.fråganr + 1)));
     sessionStorage.setItem(question.fråganr, checked);
   }
@@ -36,7 +34,7 @@ const Questions = () => {
   function handlePrevious() {
     let array = Array(11).fill(false);
     SetChecked(array);
-    getFromSession(question.fråganr -1);
+    getFromSession(question.fråganr - 1);
     SetQuestion(JSON.parse(localStorage.getItem(question.fråganr - 1)));
     sessionStorage.setItem(question.fråganr, checked);
   }
@@ -52,29 +50,74 @@ const Questions = () => {
     }
   }
 
-  function handleQuestionChoice(choice){
-     let array = Array(11).fill(false);
-     SetChecked(array);
-    getFromSession(choice)
+  function handleQuestionChoice(choice) {
+    console.log(choice);
+    let array = Array(11).fill(false);
+    SetChecked(array);
+    getFromSession(choice);
     SetQuestion(JSON.parse(localStorage.getItem(choice)));
     sessionStorage.setItem(question.fråganr, checked);
+  }
 
+  async function handleLastQuestion(){
+    let array = Array(11).fill(false);
+    SetChecked(array);
+    sessionStorage.setItem(question.fråganr, checked);
 
-
-    // let array=[...starInDropDown]
-    // array[choice]=!array[choice]
-    //  array.map((item) =>
-    //    item != true || item != false ? (item = false) : (item = item)
-    //  );
-    
-    // setStarInDropDown(array)
-    // console.log(choice)
   }
 
   function handleAbort() {}
-  function handleFinishTest() {}
+
+  async function handleFinishTest() {
+    
+    await handleLastQuestion();
+
+    //get all results unfinished
+    const resultarray = localStorageCount.map((item) => [
+      sessionStorage.getItem(item + 1),
+    ]);
+    console.log(resultarray);
+
+    let array1 = resultarray.map((item) =>
+      item.map((item2) => item2.split(","))
+    );
+    console.log(array1);
+
+    let array2 = array1.map((item) =>
+      item.map((item2) =>
+        item2.map((item3, index) =>
+          item3 == "true"
+            ? (item3 = String.fromCharCode(97 + index))
+            : (item3 = false)
+        )
+      )
+    );
+
+    let cleanUpArr = array2.map((item) =>
+      item.map((item2) => item2.filter(Boolean))
+    );
+    
+    console.log(cleanUpArr);
+
+
+    //get all questions
+    let questionArray = localStorageCount.map((item) =>
+      JSON.parse(localStorage.getItem(item + 1))
+    );
+    console.log(questionArray);
+    questionArray.map((index) =>
+      alphabet.map((letter) =>
+        index["frågealternativ" + letter] != ""
+          ? console.log("x")
+          : console.log("y")
+      )
+    );
+
+    questionArray.map((item) => console.log(item.svar));
+  }
 
   function handleChecked(index) {
+    SetChecked(Array().fill(false));
     let array = [...checked];
     array[index] = !array[index];
     array.map((item) =>
@@ -84,31 +127,46 @@ const Questions = () => {
   }
 
   const handleStar = () => {
-    setStar((star) => !star);
+    let array = [...starInDropDown];
+    array[question.fråganr - 1] = !array[question.fråganr - 1];
+    array.map((item) =>
+      item != true || item != false ? (item = false) : (item = item)
+    );
+
+    setStarInDropDown(array);
   };
 
   return (
     <>
       <div className="questionsmain">
-        <p>1</p>
+        <p></p>
         <p>{useTimer.onExpire}</p>
         <div>
-          <select onChange={(e)=>handleQuestionChoice(e.target.value)} >{localStorageCount.map((item)=><option key={item+1} value={item+1}>Fråga {item+1}</option>)}
-            
-          </select>
           <UseTimer expiryTimestamp={time} />
+          <select
+            className="questiondropdown"
+            onChange={(e) => handleQuestionChoice(e.target.value)}
+          >
+            {localStorageCount.map((item) => (
+              <option key={item + 1} value={item + 1}>
+                Fråga {item + 1} {starInDropDown[item] ? "★" : " "}
+              </option>
+            ))}
+          </select>
         </div>
         <div className="questionscontainer">
           <div className="questionsblock">
-            {star ? (
+            {starInDropDown[question.fråganr - 1] ? (
               <BsStarFill className="handlestar" onClick={handleStar} />
             ) : (
               <BsStar className="handlestar" onClick={handleStar} />
             )}
             <div className="questionsblockinner">
-              <h3 className="questionnumber">Fråga {question.fråganr}</h3>
+              <h3 className="questionnumber ralewayweight500">
+                Fråga {question.fråganr}
+              </h3>
               <div className="question setfontsize">{question.fråga}</div>
-              <div className="setfontsize">
+              <div className="setfontsize choosetext">
                 Välj ett eller flera av svaren nedan
               </div>
               <div className="questionscrollcontainer setfontsize">
@@ -155,6 +213,6 @@ const Questions = () => {
       </div>
     </>
   );
-};;
+};
 
 export default Questions;
